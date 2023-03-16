d4dc2249c72cc863221a63df80ef4b20b7b74e5c
antant200 <antonmarkelov20020605@yandex.ru>
2023-03-16 22:55:00 +0700

    Исправил ошибки 2

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -2,21 +2,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WordsChecker {
-    private   String text;
-    private String [] words = text.split("\\P{IsAlphabetic}+");
-    private Set <String> set = new HashSet<>();
-    public WordsChecker (String text){
-        this.text=text;
-    }
-    public boolean hasWord (String word){
-        for (String newWord:words) {
+    private Set<String> set = new HashSet<>();
+
+    public WordsChecker(String text) {
+        String[] words = text.split("\\P{IsAlphabetic}+");
+        for (String newWord : words) {
             set.add(newWord);
         }
-        if (set.contains(word)){
-            System.out.println(true);
+    }
+
+    public boolean hasWord(String word) {
+        if (set.contains(word)) {
+            System.out.println("Cлово "+ word +" есть!");
             return true;
         }
-        System.out.println(false);
+        System.out.println("Cлова " + word + " нет(");
         return false;
     }
 }
